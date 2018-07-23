aa1b1bedc9d09a6c396ffe241df2acec90cce62b
Alex Lunyov <isuntc@gmail.com>
2018-07-23 13:47:01 +0800

    Get rid of regeneratorRuntime

--- src/utils/getBridgeTokens.js
+++ src/utils/getBridgeTokens.js
@@ -9,25 +9,37 @@ import getWeb3 from '../utils/getWeb3';
 import { bridge as bridgeAbi, token as tokenAbi } from '../utils/abis';
 import { range } from '../utils';
 
-export default async bridgeAddress => {
+const tokenInfo = address => {
+  const iWeb3 = getWeb3(true);
+  const token = new iWeb3.eth.Contract(tokenAbi, address);
+  return Promise.all([
+    token.methods.symbol().call(),
+    token.methods.decimals().call(),
+    token.methods.name().call(),
+  ]).then(([symbol, decimals, name]) => ({
+    address,
+    symbol,
+    decimals: Number(decimals),
+    name,
+  }));
+};
+
+export default bridgeAddress => {
   const iWeb3 = getWeb3(true);
   const bridge = new iWeb3.eth.Contract(bridgeAbi, bridgeAddress);
-  const tokenCount = await bridge.methods.tokenCount().call();
-  const tokensPromises = range(0, tokenCount - 1).map(async pos => {
-    const tokenRecord = await bridge.methods.tokens(pos).call();
-    const token = new iWeb3.eth.Contract(tokenAbi, tokenRecord[0]);
-    const [symbol, decimals, name] = await Promise.all([
-      token.methods.symbol().call(),
-      token.methods.decimals().call(),
-      token.methods.name().call(),
-    ]);
-    return {
-      address: tokenRecord[0],
-      symbol,
-      decimals: Number(decimals),
-      name,
-      color: pos,
-    };
-  });
-  return Promise.all(tokensPromises);
+  return bridge.methods
+    .tokenCount()
+    .call()
+    .then(tokenCount =>
+      Promise.all(
+        range(0, tokenCount - 1).map(pos =>
+          bridge.methods
+            .tokens(pos)
+            .call()
+            .then(tokenRecord => tokenRecord[0])
+            .then(tokenInfo)
+            .then(token => Object.assign(token, { color: pos }))
+        )
+      )
+    );
 };
