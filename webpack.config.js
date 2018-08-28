f3b4505237aa43289d0802eef0dbe0d9f2bd8b7c
Alex Lunyov <isuntc@gmail.com>
2018-08-28 13:05:12 +0800

    Dedupe bn.js, replace react with preact

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,18 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
   },
+  resolve: {
+    extensions: ['.js', '.ts'],
+    alias: {
+      'bn.js': require.resolve('bn.js'),
+      react: 'preact-compat',
+      'react-dom': 'preact-compat',
+      // Not necessary unless you consume a module using `createClass`
+      'create-react-class': 'preact-compat/lib/create-react-class',
+      // Not necessary unless you consume a module requiring `react-dom-factories`
+      'react-dom-factories': 'preact-compat/lib/react-dom-factories',
+    },
+  },
   module: {
     rules: [
       {
@@ -135,9 +147,6 @@ module.exports = {
         use: ['file-loader'],
       },
     ],
-  },
-  resolve: {
-    extensions: ['.js', '.ts'],
   },
   plugins: [
     new UglifyJsPlugin({
