7b9a56d447c2f76857de4e249c9012c21edd7e52
Alex Lunyov <isuntc@gmail.com>
2018-09-28 13:26:29 +0800

    Prevent latestBlock search for search route

--- src/stores/explorer.ts
+++ src/stores/explorer.ts
@@ -5,10 +5,8 @@
  * found in the LICENSE file in the root directory of this source tree.
  */
 
-
 import Web3 from 'web3';
-import { observable, action, computed } from 'mobx';
-import autobind from 'autobind-decorator';
+import { observable, computed } from 'mobx';
 import getParsecWeb3 from '../utils/getParsecWeb3';
 import { Tx } from 'parsec-lib';
 
@@ -18,18 +16,22 @@ export default class Explorer {
   private blockchain;
   private latestBlock: number;
 
-  @observable public initialSync: boolean;
-  @observable public searching: boolean;
-  @observable public success: boolean;
+  @observable
+  public initialSync: boolean;
+  @observable
+  public searching: boolean;
+  @observable
+  public success: boolean;
 
-  @observable public current;
+  @observable
+  public current;
 
   constructor() {
     this.cache = {};
     this.blockchain = [];
     this.web3 = getParsecWeb3();
     this.initialSync = true;
-    this.init(); 
+    this.init();
   }
 
   public async search(hashOrNumber) {
@@ -40,22 +42,23 @@ export default class Explorer {
     } else {
       result = await this.getBlockOrTx(hashOrNumber);
     }
-    this.success = result ? true : false;
-    this.current = result ? result : this.current;
+    this.success = !!result;
+    this.current = result || this.current;
     this.searching = false;
-    console.log(this.current);
   }
 
   @computed
   public get currentType() {
-    if (this.current.uncles) {
-      return "BLOCK";
-    }
-    if (this.current.value) {
-      return "TRANSACTION";
-    }
-    if (this.current.balance) {
-      return "ADDRESS";
+    if (this.current) {
+      if (this.current.uncles) {
+        return 'BLOCK';
+      }
+      if (this.current.value) {
+        return 'TRANSACTION';
+      }
+      if (this.current.balance) {
+        return 'ADDRESS';
+      }
     }
     return undefined;
   }
@@ -64,58 +67,72 @@ export default class Explorer {
     this.latestBlock = await this.web3.eth.getBlockNumber();
     const blocks = [];
     for (var i = 2; i <= this.latestBlock; i++) {
-        blocks.push(i);
+      blocks.push(i);
     }
-    this.blockchain = await Promise.all(blocks.map(nr => this.getBlockOrTx(nr)));
-    await this.search(this.latestBlock);
+    this.blockchain = await Promise.all(
+      blocks.map(nr => this.getBlockOrTx(nr))
+    );
+    if (!this.current) {
+      await this.search(this.latestBlock);
+    }
     this.initialSync = false;
   }
 
   private async getAddress(address) {
     const balance = await this.web3.eth.getBalance(address);
-    const txs = this.blockchain.map(block => {
-      return block.transactions.map(tx => {
-        return (tx.from == address || tx.to == address) ? tx : undefined;
-      });
-    }).reduce((pre, cur) => {
-      return pre.concat(cur);
-    }).filter(ele => ele);
+    const txs = this.blockchain
+      .map(block => {
+        return block.transactions.map(tx => {
+          return tx.from == address || tx.to == address ? tx : undefined;
+        });
+      })
+      .reduce((pre, cur) => {
+        return pre.concat(cur);
+      })
+      .filter(ele => ele);
     const result = {
       address: address,
       balance: balance,
-      txs: txs, 
-    }
+      txs: txs,
+    };
     return result;
   }
 
   private async getBlockOrTx(hashOrNumber) {
-    if(this.cache[hashOrNumber]) {
+    if (this.cache[hashOrNumber]) {
       return this.cache[hashOrNumber];
     }
-    if (localStorage.getItem("PSC:" + hashOrNumber)) {
-      const blockOrTx = JSON.parse(localStorage.getItem("PSC:" + hashOrNumber));
+    if (localStorage.getItem('PSC:' + hashOrNumber)) {
+      const blockOrTx = JSON.parse(localStorage.getItem('PSC:' + hashOrNumber));
       this.cache[hashOrNumber] = blockOrTx;
       return blockOrTx;
     }
     let block, tx;
-    try{block = await this.web3.eth.getBlock(hashOrNumber, true);}catch{};
-    try{tx = await this.web3.eth.getTransaction(hashOrNumber);}catch{};
+    try {
+      block = await this.web3.eth.getBlock(hashOrNumber, true);
+    } catch {}
+    try {
+      tx = await this.web3.eth.getTransaction(hashOrNumber);
+    } catch {}
 
     if (block) {
-      localStorage.setItem("PSC:" + block.number.toString(), JSON.stringify(block));
-      localStorage.setItem("PSC:" + block.hash, JSON.stringify(block));
+      localStorage.setItem(
+        'PSC:' + block.number.toString(),
+        JSON.stringify(block)
+      );
+      localStorage.setItem('PSC:' + block.hash, JSON.stringify(block));
       this.cache[block.number.toString()] = block;
       this.cache[block.hash] = block;
       block.transactions.map(tx => {
-        tx = {...tx, ...Tx.fromRaw(tx.raw)};
-        localStorage.setItem("PSC:" + tx.hash, JSON.stringify(tx));
+        tx = { ...tx, ...Tx.fromRaw(tx.raw) };
+        localStorage.setItem('PSC:' + tx.hash, JSON.stringify(tx));
         this.cache[tx.hash] = tx;
-      })
+      });
       return block;
     }
     if (tx) {
-      tx = {...tx, ...Tx.fromRaw(tx.raw)};
-      localStorage.setItem("PSC:" + hashOrNumber, JSON.stringify(tx));
+      tx = { ...tx, ...Tx.fromRaw(tx.raw) };
+      localStorage.setItem('PSC:' + hashOrNumber, JSON.stringify(tx));
       this.cache[hashOrNumber] = tx;
       return tx;
     }
