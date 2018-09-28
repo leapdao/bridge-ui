b1279aba1eff5d97d5d492bb8e40a1fc5b25fef3
Alex Lunyov <isuntc@gmail.com>
2018-09-28 14:49:00 +0800

    Improve formatting and styles

--- src/components/explorer/block.js
+++ src/components/explorer/block.js
@@ -6,23 +6,35 @@ import { Link } from 'react-router-dom';
 
 import TransctionList from './transactionList';
 
+const dateFormat = new Intl.DateTimeFormat('en-US', {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  second: 'numeric',
+  hour12: false,
+});
+
 const Block = ({ explorer }) => {
   const block = explorer.current;
   const title = `Block ${block.number}`;
   return (
-    <Card title={title}>
-      <h3> Hash: </h3>
+    <Card title={title} className="explorer-section">
+      <div className="explorer-nav">
+        <Button>
+          <Link to={`/explorer/${block.number - 1}`}> Prev </Link>
+        </Button>
+        <Button>
+          <Link to={`/explorer/${block.number + 1}`}> Next </Link>
+        </Button>
+      </div>
+      <h3>Hash:</h3>
       {block.hash}
-      <h3> Timestamp: </h3>
-      {block.timestamp}
-      <h3> Transactions: </h3>
+      <h3>Timestamp:</h3>
+      {dateFormat.format(block.timestamp * 1000)}
+      <h3>Transactions:</h3>
       <TransctionList txs={block.transactions} />
-      <Button>
-        <Link to={`/explorer/${block.number - 1}`}> Prev </Link>
-      </Button>
-      <Button>
-        <Link to={`/explorer/${block.number + 1}`}> Next </Link>
-      </Button>
     </Card>
   );
 };
--- src/components/explorer/transaction.js
+++ src/components/explorer/transaction.js
@@ -9,37 +9,39 @@ import Searchable from './searchable';
 const Transaction = ({ explorer }) => {
   const tx = explorer.current;
   return (
-    <Card title="Transaction">
-      <h3> Hash: </h3>
+    <Card title="Transaction" className="explorer-section">
+      <h3>Hash:</h3>
       {tx.hash}
-      <h3> Block: </h3>
+      <h3>Block:</h3>
       <Searchable text={tx.blockHash} />
-      <h3> ID in block: </h3>
+      <h3>ID in block:</h3>
       {tx.transactionIndex}
-      <h3> From: </h3>
+      <h3>From:</h3>
       <Searchable text={tx.from} />
-      <h3> To: </h3>
+      <h3>To:</h3>
       <Searchable text={tx.to} />
-      <h3> Value: </h3>
+      <h3>Value:</h3>
       {tx.value}
-      <h3> Inputs: </h3>
+      <h3>Inputs:</h3>
       <List
         itemLayout="vertical"
         dataSource={tx.inputs}
         renderItem={input => (
           <List.Item>
-            <Searchable text={input.hash} /> : {input.index}
+            <Searchable text={input.hash} />: {input.index}
           </List.Item>
         )}
       />
-      <h3> Outputs: </h3>
+      <h3>Outputs:</h3>
       <List
         itemLayout="vertical"
         dataSource={tx.outputs}
         renderItem={output => (
           <List.Item>
             Address: <Searchable text={output.address} />
+            <br />
             Color: {output.color}
+            <br />
             Value: {output.value}
           </List.Item>
         )}
