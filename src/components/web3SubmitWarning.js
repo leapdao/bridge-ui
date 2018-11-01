b2c5d5b3978fe13e5e29d633d56a8585a67e8665
Alex Lunyov <isuntc@gmail.com>
2018-11-01 14:35:57 +0800

    Show connect button in web3SubmitWarning

--- src/components/web3SubmitWarning.js
+++ src/components/web3SubmitWarning.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { inject, observer } from 'mobx-react';
 import PropTypes from 'prop-types';
-import { Alert } from 'antd';
+import { Alert, Button } from 'antd';
 import { NETWORKS } from '../utils';
 
-@inject(stores => ({
-  network: stores.network,
-  account: stores.account,
-}))
+@inject('network', 'account', 'web3')
 @observer
 class Web3SubmitWarning extends React.Component {
   render() {
-    const { network, account } = this.props;
+    const { network, account, web3 } = this.props;
 
-    if (!window.web3) {
+    if (!web3.injectedAvailable) {
       return (
         <Alert
           type="warning"
           style={{ marginBottom: 10 }}
           message="To be able to send transactions you need to install MetaMask"
+        />
+      );
+    }
+
+    if (!web3.injected) {
+      return (
+        <Alert
+          type="warning"
+          style={{ marginBottom: 10 }}
+          message={
+            <Fragment>
+              To be able to send transactions you need to{' '}
+              <Button onClick={() => web3.enable()}>
+                connect MetaMask{' '}
+                <span role="img" aria-label="fox">
+                  🦊
+                </span>
+              </Button>
+            </Fragment>
+          }
         />
       );
     }
@@ -52,6 +69,7 @@ class Web3SubmitWarning extends React.Component {
 Web3SubmitWarning.propTypes = {
   network: PropTypes.object,
   account: PropTypes.object,
+  web3: PropTypes.object,
 };
 
 export default Web3SubmitWarning;
