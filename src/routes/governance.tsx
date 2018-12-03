8e5b7281740c4ae55e89690f4e765b3e18f7786a
Kosta Korenkov <kosta@whoot.me>
2018-12-03 17:04:52 +0100

    add title attribute to shortened links

--- src/routes/governance.tsx
+++ src/routes/governance.tsx
@@ -97,7 +97,7 @@ export default class Governance extends React.Component<GovernanceProps, any> {
     const etherscanLink = value => `${NETWORKS[this.props.network.network].etherscanBase}/address/${value}`;
 
     return (
-      <a href={etherscanLink(value)}>
+      <a href={etherscanLink(value)} title={value}>
         {shortenedValue}
       </a>
     );
