a6f3419200e4c7c650966e3957da59344f110a7d
Kosta Korenkov <kosta@whoot.me>
2019-01-12 18:23:28 +0300

    fix: Bridge store is trying to read slots

--- src/stores/bridge.ts
+++ src/stores/bridge.ts
@@ -5,71 +5,16 @@
  * found in the LICENSE file in the root directory of this source tree.
  */
 
-import { observable, action, reaction, IObservableArray } from 'mobx';
-import autobind from 'autobind-decorator';
-import Contract from 'web3/eth/contract';
 import { bridge as bridgeAbi } from '../utils/abis';
 
-import Token from './token';
-import Slot from './slot';
-import Account from './account';
 import ContractStore from './contractStore';
 import Transactions from '../components/txNotification/transactions';
 
-import { range } from '../utils';
-import { InflightTxReceipt } from '../utils/types';
 import Web3Store from './web3';
 
-const readSlots = (bridge: Contract) => {
-  return bridge.methods
-    .epochLength()
-    .call()
-    .then(epochLength =>
-      Promise.all(
-        range(0, epochLength - 1).map(slotId =>
-          bridge.methods.getSlot(slotId).call()
-        )
-      )
-    )
-    .then(slots => {
-      return slots.map(
-        ({
-          0: eventCounter,
-          1: owner,
-          2: stake,
-          3: signer,
-          4: tendermint,
-          5: activationEpoch,
-          6: newOwner,
-          7: newStake,
-          8: newSigner,
-          9: newTendermint,
-        }) =>
-          new Slot({
-            eventCounter,
-            owner,
-            stake: Number(stake),
-            signer,
-            tendermint,
-            activationEpoch: Number(activationEpoch),
-            newOwner,
-            newStake: Number(newStake),
-            newSigner,
-            newTendermint,
-          })
-      );
-    });
-};
-
 export default class Bridge extends ContractStore {
-  @observable
-  public slots: IObservableArray<Slot> = observable.array([]);
-
-  @observable
-  public lastCompleteEpoch: number;
 
   constructor(
-    private account: Account,
     transactions: Transactions,
     web3: Web3Store,
     address?: string
@@ -79,116 +24,6 @@ export default class Bridge extends ContractStore {
     web3.plasma.getConfig().then(({ bridgeAddr }) => {
       this.address = bridgeAddr;
     });
-
-    reaction(() => {
-      return this.contract;
-    }, this.init);
-    reaction(
-      () => this.events,
-      () => {
-        this.events.on('allEvents', this.loadContractData.bind(this));
-      }
-    );
   }
 
-  @autobind
-  private init() {
-    this.loadContractData();
-  }
-
-  @autobind
-  @action
-  private updateData([slots, lastCompleteEpoch]: [Array<Slot>, string]) {
-    this.slots = observable.array(slots);
-    this.lastCompleteEpoch = Number(lastCompleteEpoch);
-  }
-
-  private loadContractData() {
-    Promise.all([
-      readSlots(this.contract),
-      this.contract.methods.lastCompleteEpoch().call(),
-    ]).then(this.updateData);
-  }
-
-  public deposit(token: Token, amount: any): Promise<InflightTxReceipt> {
-    if (!this.iContract) {
-      throw new Error('No metamask');
-    }
-
-    const data = this.contract.methods
-      .deposit(this.account.address, amount, token.color)
-      .encodeABI();
-
-    const inflightTxReceiptPromise = token.approveAndCall(
-      this.address,
-      amount,
-      data
-    );
-
-    this.watchTx(inflightTxReceiptPromise, 'deposit', {
-      message: 'Deposit tokens to the bridge',
-    });
-
-    return inflightTxReceiptPromise;
-  }
-
-  public bet(
-    token: Token,
-    slotId: number,
-    stake: any,
-    signerAddr: string,
-    tendermint: string
-  ): Promise<InflightTxReceipt> {
-    const data = this.contract.methods
-      .bet(slotId, stake, signerAddr, `0x${tendermint}`)
-      .encodeABI();
-
-    const inflightTxReceiptPromise = token.approveAndCall(
-      this.address,
-      stake,
-      data
-    );
-
-    this.watchTx(inflightTxReceiptPromise, 'bet', {
-      message: 'Place a bet to the bridge contract',
-    });
-
-    return inflightTxReceiptPromise;
-  }
-
-  public registerToken(tokenAddr: string) {
-    const tx = this.iContract.methods.registerToken(tokenAddr).send({
-      from: this.account.address,
-    });
-
-    this.watchTx(tx, 'registerToken', {
-      message: 'Register a new token on the bridge',
-    });
-
-    return tx;
-  }
-
-  public startExit(proof: string[], outIndex: number) {
-    const tx = this.iContract.methods.startExit(proof, outIndex).send({
-      from: this.account.address,
-    });
-
-    this.watchTx(tx, 'startExit', {
-      message: 'Exit',
-    });
-
-    return tx;
-  }
-
-  public finalizeExits(color: number) {
-    const tx = this.iContract.methods.finalizeExits(color).send({
-      from: this.account.address,
-    });
-
-    this.watchTx(tx, 'finalizeExits', {
-      message: 'Finalize exits',
-    });
-
-    return tx;
-  }
 }
