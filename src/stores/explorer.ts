5c538e220c0f9d72d9dd846fd47f620b38b05eb9
Kosta Korenkov <kosta@whoot.me>
2019-01-21 21:02:18 +0300

    do not download blockchain on the client.
    If the chain has thousadns of blocks it will cripple the browser with a flux of XHR and also overuse the localStorage.
    The whole chian is only needed to calculate txs for token/address — disabling this for a time being.

--- src/stores/explorer.ts
+++ src/stores/explorer.ts
@@ -6,10 +6,8 @@
  */
 
 import { observable, computed, reaction } from 'mobx';
-import { Tx, TxJSON } from 'leap-core';
-import { Block, Transaction } from 'web3/eth/types';
-import { range } from '../utils/range';
-import autobind from 'autobind-decorator';
+import { Tx, TxJSON, LeapTransaction } from 'leap-core';
+import { Block } from 'web3/eth/types';
 import NodeStore from './node';
 import Web3Store from './web3/';
 import Tokens from './tokens';
@@ -22,42 +20,21 @@ export enum Types {
   ADDRESS,
 }
 
-type PlasmaTransaction = Transaction & {
-  raw: string;
-  color: number;
-} & TxJSON;
+type PlasmaTransaction = LeapTransaction & TxJSON;
 
 type PlasmaBlock = Block & {
   transactions: PlasmaTransaction[];
 } & TxJSON;
 
-const accountTransaction = (address: string) => {
-  address = address.toLowerCase();
-  return (tx: PlasmaTransaction) => {
-    const from = (tx.from || '').toLowerCase();
-    const to = (tx.to || '').toLowerCase();
-    return from === address || to === address;
-  };
-};
-
-const tokenTransaction = (color: number) => {
-  return (tx: PlasmaTransaction) => tx.color === color;
-};
-
 export default class Explorer {
-  private blockchain: Block[] = [];
   private _cache = {};
 
-  @observable
-  private latestBlock: number = 0;
 
   @observable
   public searching: boolean = false;
 
   @observable
   public syncing: boolean = false;
-
-  private syncingPromise: Promise<Block[]>;
 
   @observable
   public success: boolean = true;
@@ -66,7 +43,6 @@ export default class Explorer {
   public current;
 
   constructor(
-    private node: NodeStore,
     private web3: Web3Store,
     private tokens: Tokens
   ) {
@@ -77,9 +53,6 @@ export default class Explorer {
       }
     } catch (e) {}
 
-    reaction(() => this.node.latestBlock, this.getBlockchain);
-    // this.getBlockchain();
-    // setInterval(this.getBlockchain, 5000);
   }
 
   private get cache() {
@@ -89,31 +62,6 @@ export default class Explorer {
   private setCache(key, value) {
     this._cache[key] = value;
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(this._cache));
-  }
-
-  @autobind
-  private getBlockchain(): Promise<Block[]> {
-    this.syncing = true;
-    if (this.syncingPromise) {
-      return this.syncingPromise;
-    }
-    if (this.latestBlock === this.node.latestBlock) {
-      return Promise.resolve(this.blockchain);
-    }
-
-    const fromBlock = this.latestBlock;
-    this.latestBlock = this.node.latestBlock;
-
-    this.syncingPromise = Promise.all(
-      range(fromBlock, this.latestBlock).map(n => this.getBlock(n))
-    ).then(blocks => {
-      this.blockchain = this.blockchain.concat(blocks);
-      this.syncing = false;
-      this.syncingPromise = null;
-      return this.blockchain;
-    });
-
-    return this.syncingPromise;
   }
 
   private static getType(obj) {
@@ -138,26 +86,11 @@ export default class Explorer {
 
   public getAddress(address: string) {
     address = address.toLowerCase();
-    console.log(
-      this.tokens.tokenForAddress(address),
-      this.tokens.tokenIndexForAddress(address)
-    );
     const token = this.tokens.tokenForAddress(address);
     return Promise.all([
       this.web3.plasma.instance.eth.getBalance(address),
-      this.getBlockchain(),
-    ]).then(([balance, blocks]) => {
-      const txs = blocks.reduce(
-        (accum, block) =>
-          accum.concat(
-            block.transactions.filter(
-              token
-                ? tokenTransaction(token.color)
-                : accountTransaction(address)
-            )
-          ),
-        [] as Transaction[]
-      );
+      this.getTransactionsByAddress(address, token),
+    ]).then(([balance, txs]) => {
       return {
         address,
         token,
@@ -246,5 +179,12 @@ export default class Explorer {
           }
         );
     }
+  }
+
+  private getTransactionsByAddress(token, address): Promise<LeapTransaction[]> {
+    // TODO: probably should be fetched from some caching layer
+    // it is infeasible to calculate this on client-side because the chain may be huge
+    console.warn('Not implemented');
+    return Promise.resolve([]);
   }
 }
