51486806651c516158027e16cd76a9e30b233912
Kosta Korenkov <kosta@whoot.me>
2019-02-09 16:46:20 +0300

    whitelist icons for governance page (#152)
    * whitelist icons for governance page
    
    * add missing Right arrow icon to prod bundle
    
    * fix: css styles apply in the wrong order in prod bundle

--- src/icons.js
+++ src/icons.js
@@ -22,3 +22,12 @@ export {
 export {
   default as CloseCircleFill,
 } from '@ant-design/icons/lib/fill/CloseCircleFill';
+export {
+  default as IssuesCloseOutline,
+} from '@ant-design/icons/lib/outline/IssuesCloseOutline';
+export {
+  default as ClockCircleOutline,
+} from '@ant-design/icons/lib/outline/ClockCircleOutline';
+export {
+  default as RightOutline,
+} from '@ant-design/icons/lib/outline/RightOutline';
