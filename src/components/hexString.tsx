aafa00875983b7ea4359b33c9909a836d2c8df96
Alex Lunyov <isuntc@gmail.com>
2019-02-12 23:20:28 +0800

    Cleaning

--- src/components/hexString.tsx
+++ src/components/hexString.tsx
@@ -9,18 +9,6 @@ interface HexStringProps {}
 
 const HexString: React.SFC<HexStringProps> = ({ children }) => {
   const width = String(children).length < 60 ? 450 : 600;
-  // {shortenHex(children)}
-  /**
-   * length < 60
-   * ----
-   * width >= 500 — full
-   * width < 500 — short
-   *
-   * length >= 60
-   * ----
-   * width >= 600
-   * width < 600
-   */
   return (
     <Fragment>
       <MediaQuery minWidth={width}>{children}</MediaQuery>
