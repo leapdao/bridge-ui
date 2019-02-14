6b56d818a161d71e8d53c903e2878f800a53cff6
Alex Lunyov <isuntc@gmail.com>
2019-02-14 12:54:40 +0800

    Copy shorten hex to clipboard

--- src/components/hexString.tsx
+++ src/components/hexString.tsx
@@ -7,13 +7,46 @@ const { Fragment } = React;
 
 interface HexStringProps {}
 
+function copyToClipboard(text: string) {
+  const textArea = document.createElement('textarea');
+  textArea.value = text;
+  document.body.appendChild(textArea);
+  textArea.select();
+  const result = document.execCommand('copy');
+  document.body.removeChild(textArea);
+  return result;
+}
+
 const HexString: React.SFC<HexStringProps> = ({ children }) => {
+  const [copied, setCopied] = React.useState(false);
+  const handleCopy = React.useCallback(() => {
+    if (copyToClipboard(String(children))) {
+      setCopied(true);
+    }
+  }, [children]);
+
+  React.useEffect(() => {
+    if (copied) {
+      const timeout = setTimeout(() => {
+        setCopied(false);
+      }, 400);
+
+      return () => {
+        clearTimeout(timeout);
+      };
+    }
+  }, [copied]);
+
   const width = String(children).length < 60 ? 450 : 600;
+
   return (
     <Fragment>
+      {copied && <span className="copied-msg">Copied</span>}
       <MediaQuery minWidth={width}>{children}</MediaQuery>
       <MediaQuery maxWidth={width - 1}>
-        <span title={String(children)}>{shortenHex(children)}</span>
+        <span title={String(children)} onClick={handleCopy}>
+          {shortenHex(children)}
+        </span>
       </MediaQuery>
     </Fragment>
   );
