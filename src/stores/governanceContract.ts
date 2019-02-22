0c4157a0a08092f0c710d1fc54a6e8bf90ee4a74
StevenJNPearce <36509712+StevenJNPearce@users.noreply.github.com>
2019-02-23 01:17:04 +0800

    add "Finalize" button on Governance page (#175)
    * add "Finalize" button on Governance page
    
    * fix

--- src/stores/governanceContract.ts
+++ src/stores/governanceContract.ts
@@ -4,7 +4,7 @@
  * This source code is licensed under the GNU GENERAL PUBLIC LICENSE Version 3
  * found in the LICENSE file in the root directory of this source tree.
  */
-import { reaction, observable, IObservableArray } from 'mobx';
+import { reaction, observable, IObservableArray, computed } from 'mobx';
 
 import Web3Store from './web3/';
 import { ABIDefinition, EthAbiDecodeParametersResultObject } from 'web3/eth/abi';
@@ -90,6 +90,14 @@ export default class GovernanceContract extends ContractStore {
   @observable
   public proposals: IObservableArray<Proposal>;
 
+  @computed
+  public get canFinalize() : boolean {
+    return this.proposals && this.proposals.filter(proposal => {
+      const date = new Date();
+      return proposal.effectiveDate <= date.getTime() 
+    }).length > 0
+  }
+
   @observable
   public noGovernance: boolean;
 
@@ -111,6 +119,23 @@ export default class GovernanceContract extends ContractStore {
         })
       }
     );
+  }
+
+  finalize = async () => {
+    if (!this.iContract) {
+      console.error('Need injected web3 to finalize')
+      return;
+    }
+    const accounts = await this.web3.injected.instance.eth.getAccounts()
+    const tx = this.iContract.methods.finalize().send({
+      from: accounts[0]
+    });
+
+    this.watchTx(tx, 'finalize', {
+      message: 'Finalize proposals',
+    });
+
+    return tx;
   }
 
   private readCurrentValue(proposal, governanceChange) {
