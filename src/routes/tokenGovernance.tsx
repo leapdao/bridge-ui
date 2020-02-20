3ca6d65b824792d24642291b0e2a15c4df5d1bdf
Simon Weniger <hello@simonweniger.com>
2020-02-20 13:35:48 +0100

    added ABI + prepared the form to submit

--- src/routes/tokenGovernance.tsx
+++ src/routes/tokenGovernance.tsx
@@ -1,15 +1,14 @@
 import * as React from 'react';
 import { observer } from 'mobx-react';
-import { List, Collapse, Icon, Divider, Tabs } from 'antd';
+import { List, Collapse, Icon, Tabs } from 'antd';
 import TimeAgo from 'react-timeago';
+import Web3 from 'web3';
 
 import Web3SubmitWarning from '../components/web3SubmitWarning';
 
-import AppLayout from '../components/appLayout';
 import EtherscanLink from '../components/etherscanLink';
 import { shortenHex } from '../utils';
 import { Button, Menu, Form, Input, Row, Col } from 'antd';
-import { governanceContractStore } from '../stores/governanceContract';
 import { reaction } from 'mobx';
 import TextArea from 'antd/lib/input/TextArea';
 
@@ -17,6 +16,19 @@ const { Fragment } = React;
 const { TabPane } = Tabs;
 
 export default class TokenGovernance extends React.Component {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      title: '',
+      description: '',
+    };
+  }
+
+  public handleSubmit(e: any) {
+    e.preventDefault();
+  }
+
   public state = {
     current: 'open',
   };
@@ -31,6 +43,9 @@ export default class TokenGovernance extends React.Component {
     function callback(key) {
       console.log(key);
     }
+
+    console.log(this.state);
+
     return (
       <Fragment>
         <h1>Token Governance</h1>
@@ -38,21 +53,25 @@ export default class TokenGovernance extends React.Component {
 
         <Tabs defaultActiveKey="1" onChange={callback} type="card">
           <TabPane tab="Open for voting" key="1">
-            Content of Tab Pane 1
+            Content of Tab d1
           </TabPane>
           <TabPane tab="Ready for processing" key="2">
-            Content of Tab Pane 2
+            Content of Tab 2
           </TabPane>
           <TabPane tab="Finalize proposals" key="3">
-            Content of Tab Pane 3
+            Content of Tab 3
           </TabPane>
           <TabPane tab="Submit a proposal" key="4">
-            <Form>
+            <Form onSubmit={e => this.handleSubmit(e)}>
               <Row gutter={28}>
                 <Col span={10}>
                   <h3>Proposal Title</h3>
                   <Form.Item>
-                    <Input placeholder="Give your proposal a Title" />
+                    <Input
+                      name="title"
+                      placeholder="Give your proposal a Title"
+                      onChange={e => this.setState({ title: e.target.value })}
+                    />
                   </Form.Item>
                 </Col>
               </Row>
@@ -61,8 +80,12 @@ export default class TokenGovernance extends React.Component {
                   <h3>Proposal Description</h3>
                   <Form.Item>
                     <TextArea
+                      name="description"
                       rows={4}
                       placeholder="Describe what your proposal is about..."
+                      onChange={e =>
+                        this.setState({ description: e.target.value })
+                      }
                     />
                   </Form.Item>
                 </Col>
