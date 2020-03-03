81a3f9bc697623b813a909d50f74b86496648915
Kosta Korenkov <kosta@whoot.me>
2020-03-03 23:04:33 +0700

    refactor: cleanup first PR for token governance (#276)
    - move token governance contract address into network config
    - remove setState in favour of MobX's @observable + @observer
    - better message for registerProposal popup helper
    - minor changes

--- src/routes/tokenGovernance.tsx
+++ src/routes/tokenGovernance.tsx
@@ -1,84 +1,66 @@
 import * as React from 'react';
-import { Tabs } from 'antd';
+import { observable, computed } from 'mobx';
+import { observer } from 'mobx-react';
+import { Button, Form, Input, Row, Col, Tabs } from 'antd';
 
-import Web3SubmitWarning from '../components/web3SubmitWarning';
-
-import { Button, Form, Input, Row, Col } from 'antd';
-import TextArea from 'antd/lib/input/TextArea';
-import { keccak256 } from 'ethereumjs-util';
-import { observable, computed, reaction } from 'mobx';
+import { keccak256, bufferToHex } from 'ethereumjs-util';
 import Contract from 'web3/eth/contract';
 import autobind from 'autobind-decorator';
+import Web3SubmitWarning from '../components/web3SubmitWarning';
 import { web3InjectedStore } from '../stores/web3/injected';
 import { tokenGovernance as tokenGovAbi } from '../utils/abis';
 import { governanceContractStore } from '../stores/governanceContract';
+import { CONFIG } from '../config';
 
+const { TextArea } = Input;
 const { Fragment } = React;
 const { TabPane } = Tabs;
 
+@observer
 export default class TokenGovernance extends React.Component {
   @observable
-  public key: any;
-  public state: { title: string; description: string };
-  public props: any;
+  private title: string;
 
-  constructor(props: {}) {
-    super(props);
-
-    this.state = {
-      title: '',
-      description: '',
-    };
-  }
+  @observable
+  private description: string;
 
   @computed
   public get tokenGovContract(): Contract | undefined {
     if (web3InjectedStore.instance) {
       return new web3InjectedStore.instance.eth.Contract(
         tokenGovAbi,
-        '0x3cc955f91d645b4250f6070a8b7d71365662776f'
+        CONFIG.tokenGovernanceAddress
       );
     }
   }
 
   @autobind
   public async sendToContract() {
-    console.log(
-      'title: ',
-      this.state.title,
-      'description: ',
-      this.state.description
-    );
+    console.log('title:', this.title, 'description:', this.description);
 
-    const proposalHashBuffer = keccak256(
-      `${this.state.title}::${this.state.description}`
+    const proposalHash = bufferToHex(
+      keccak256(`${this.title}::${this.description}`)
     );
-
-    const proposalHash = `0x${Buffer.from(proposalHashBuffer).toString('hex')}`;
-    console.log('tokenGovernance', proposalHash);
+    console.log({ proposalHash });
 
     const accounts = await web3InjectedStore.instance.eth.getAccounts();
 
-    console.log('proposalHash', proposalHash);
-    console.log('tokenGovernanceContract', proposalHash);
     const tx = this.tokenGovContract.methods
       .registerProposal(proposalHash)
       .send({
         from: accounts[0],
       });
-    console.log(this.tokenGovContract.methods.registerProposal(proposalHash));
 
-    governanceContractStore.watchTx(tx, 'send..', {
-      message: 'send',
+    governanceContractStore.watchTx(tx, 'registerProposal', {
+      message:
+        'Registering token governance proposal. ' +
+        'A stake of 5000 LEAP will be deducted from your account',
     });
 
     return tx;
   }
 
   public render() {
-    console.log(this.state.title);
-    console.log(this.state.description);
-
     return (
       <Fragment>
         <h1>Token Governance</h1>
@@ -102,8 +84,9 @@ export default class TokenGovernance extends React.Component {
                   <Form.Item>
                     <Input
                       name="title"
+                      value={this.title}
                       placeholder="Give your proposal a Title"
-                      onChange={e => this.setState({ title: e.target.value })}
+                      onChange={e => (this.title = e.target.value)}
                     />
                   </Form.Item>
                 </Col>
@@ -114,11 +97,10 @@ export default class TokenGovernance extends React.Component {
                   <Form.Item>
                     <TextArea
                       name="description"
+                      value={this.description}
                       rows={4}
                       placeholder="Describe what your proposal is about..."
-                      onChange={e =>
-                        this.setState({ description: e.target.value })
-                      }
+                      onChange={e => (this.description = e.target.value)}
                     />
                   </Form.Item>
                 </Col>
