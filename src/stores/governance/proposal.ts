4e7e0e587e0cdd46d71b3347162e1629dede9c49
Kosta Korenkov <kosta@whoot.me>
2020-03-07 14:55:29 +0700

    simplify Proposal object

--- src/stores/governance/proposal.ts
+++ src/stores/governance/proposal.ts
@@ -1,38 +1,21 @@
-import { computed, observable } from 'mobx';
+import { observable } from 'mobx';
 
 export class Proposal {
-  @observable
-  private _title: string = '';
-
-  @observable
-  private _description: string = '';
-
-  @observable
-  private ipfsHash: string = null;
+  @observable public readonly description: string;
+  @observable public readonly title: string;
+  @observable public readonly hash: string;
 
   constructor(title: string, description: string, hash?: string) {
-    this._title = title;
-    this._description = description;
-    this.ipfsHash = hash;
-  }
-
-  @computed get title() {
-    return this._title;
-  }
-
-  @computed get description() {
-    return this._description;
-  }
-
-  @computed get hash() {
-    return this.ipfsHash;
+    this.description = description;
+    this.title = title;
+    this.hash = hash;
   }
 
   public toString() {
     return JSON.stringify({
       title: this.title,
       description: this.description,
-      hash: this.ipfsHash,
+      hash: this.hash,
     });
   }
 }
